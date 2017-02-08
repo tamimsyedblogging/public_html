b10369d2dff9521d840d433b18105282b3460c17
TamimSyed <tamimsyedblogging@gmail.com>
2017-02-07 21:54:07 -0500

    added the timezone code

--- github.php
+++ github.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('US/Eastern');
 shell_exec('git pull');
 echo date('l jS \of F Y h:i:s A');
 echo '<p>Code updated at from Git repo.Try to refresh the site and clear your Browser cache.</p>';
