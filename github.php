a0b0c60ecf59c4180d044194f67eb8fd8c8fabb1
TamimSyed <tamimsyedblogging@gmail.com>
2017-02-07 21:49:27 -0500

    added the code changes in php files

--- github.php
+++ github.php
@@ -1,4 +1,5 @@
 <?php
 shell_exec('git pull');
-echo '<p>Code updated from Git repo.Try to refresh the site and clear your Browser cache.</p>';
+echo date('l jS \of F Y h:i:s A');
+echo '<p>Code updated at from Git repo.Try to refresh the site and clear your Browser cache.</p>';
 ?>
